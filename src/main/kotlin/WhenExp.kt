dfa5adde1187000ffad5fc79a0f3848e42bb74df
Dafa24 <akbarpradipa@gmail.com>
2020-08-25 21:40:12 +0700

    Ctrl Flow Part1

---
+++ src/main/kotlin/WhenExp.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val value = 24
+
+    when (value){
+        24 -> println("value is 24")
+        90 -> println("value is 90")
+        15 -> println("value is 15")
+        else -> println("value cannot be reached")
+    }
+}
